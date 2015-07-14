661cfdbb1c0c6477ae12b9ad0ab6399891e10599
Thomas Profitt <tomdprofitt@gmail.com>
2015-07-14 00:55:57 -0500

    Take a sad song and make it better

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -7,7 +7,7 @@ class ApiController < ApplicationController
 
   # GET
   def redirect_to_random_porn
-    redirect_to random_porn_url
+    redirect_to random_porn_url[:file_path]
   end
 
   # GET
@@ -27,55 +27,49 @@ class ApiController < ApplicationController
     saturation = 50
     lightness = 10
 
-    if hours < 5 || hours >= 23
+    case hours
+    when 0..5, 23..24
       lightness = 5
-    elsif hours < 12
+    when 5..12
       saturation =
         range_value_from_position(
-          100..52,
-          range_position_from_value(5..12, hours))
+          100..52, range_position_from_value(5..12, hours))
       lightness =
         range_value_from_position(
-          5..70,
-          range_position_from_value(5..12, hours))
-    elsif hours < 15
+          5..70, range_position_from_value(5..12, hours))
+    when 12..15
       saturation = 52
       lightness =
         range_value_from_position(
-          70..54,
-          range_position_from_value(10..15, hours))
-    elsif hours < 19
+          70..54, range_position_from_value(12..15, hours))
+    when 15..19
       hue =
         range_value_from_position(
-          200..168,
-          range_position_from_value(15..19, hours))
+          200..168, range_position_from_value(15..19, hours))
       saturation =
         range_value_from_position(
-          50..20,
-          range_position_from_value(15..19, hours))
+          50..20, range_position_from_value(15..19, hours))
       lightness =
         range_value_from_position(
-          54..45,
-          range_position_from_value(15..19, hours))
-    elsif hours < 20
+          54..45, range_position_from_value(15..19, hours))
+    when 19..20
       hue =
         range_value_from_position(
-          40..20,
-          range_position_from_value(19..20, hours))
+          40..20, range_position_from_value(19..20, hours))
       saturation =
         range_value_from_position(
-          20..76,
-          range_position_from_value(19..20, hours))
+          20..30, range_position_from_value(19..20, hours))
       lightness =
         range_value_from_position(
-          45..62,
-          range_position_from_value(19..20, hours))
-    elsif hours < 23
+          45..40, range_position_from_value(19..20, hours))
+    when 20..23
+      hue =
+        range_value_from_position(
+          20..30, range_position_from_value(20..23, hours))
+      saturation = 10
       lightness =
         range_value_from_position(
-          10..5,
-          range_position_from_value(20..23, hours))
-      # default
+          40..5, range_position_from_value(20..23, hours))
     end
 
     color = "hsl(#{hue},#{saturation}%,#{lightness}%)"
